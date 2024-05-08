968ca41fab87e96cc6c57a609afa7924b9cf3a8f
arvend <arvendrajan100@gmail.com>
2024-05-08 21:19:38 +0800

    Initial Commit

---
+++ src/main/java/com/neuroginestore/NeurogineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.neuroginestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurogineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeurogineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neuroginestore/NeurogineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuroginestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeurogineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
